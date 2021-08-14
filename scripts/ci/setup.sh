2312f28423fc9e45a5328797e54bf4e0d4d6b23b
Nick Rycar <nick@buildkite.com>
2021-08-13 22:07:05 -0700

    Be more laid back with your audits, dude.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -11,8 +11,8 @@ echo 'npm install libvips'
 npm install libvips
 echo 'npm install sharp'
 npm install sharp --build-from-source
-echo 'npm audit fix --force'
-npm audit fix --force
+echo 'npm audit fix'
+npm audit fix
 
 echo 'yarn add'
 yarn add jquery@3.5.1 bootstrap@3.4.1
