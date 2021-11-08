286583b7a4db5e1f615c5e0037eadad6166e5183
Nick Rycar <nick@buildkite.com>
2021-11-08 13:21:57 -0800

    add a sleepy sleep.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -8,3 +8,14 @@ STEP_SPAN_ID=$(echo ohai | sum | cut -f 1 -d \ )
 echo "Ohai!"
 
 ./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START ohai
+
+STEP_START=$(date +%s)
+STEP_SPAN_ID=$(echo nite-nite | sum | cut -f 1 -d \ )
+
+echo "Yaaaaaawn"
+
+sleep 10
+
+echo "I'm up!"
+
+./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START nite-nite
