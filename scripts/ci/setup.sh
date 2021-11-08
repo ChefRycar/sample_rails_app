09dc3b8da2bc12a9b2e55850b449f7a4c6321a4e
Nick Rycar <nick@buildkite.com>
2021-11-08 11:32:30 -0800

    Fix paths

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -7,4 +7,4 @@ STEP_SPAN_ID=$(echo ohai | sum | cut -f 1 -d \ )
 
 echo "Ohai!"
 
-./buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START ohai
+./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START ohai
