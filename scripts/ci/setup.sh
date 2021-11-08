7b16a0f95e5f93bcc396762c0f825ede4e9dba6b
Nick Rycar <nick@buildkite.com>
2021-11-08 13:27:03 -0800

    clean up the honeycomb tooling.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -5,17 +5,18 @@ BUILD_START=$(date +%s)
 STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo ohai | sum | cut -f 1 -d \ )
 
-echo "Ohai!"
 
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID echo "Ohai!"
 ./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START ohai
 
 STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo nite-nite | sum | cut -f 1 -d \ )
 
-echo "Yaaaaaawn"
-
-sleep 10
-
-echo "I'm up!"
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID echo "Yaaaaaawn"
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID sleep 10
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID echo "I'm up!"
 
 ./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START nite-nite
+
+# FINISH IT
+./scripts/ci/buildevents build $BUILDKITE_BUILD_ID $Build_START success
