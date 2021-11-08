3b69366ccfc012e34cb348da9153691845d57d86
Nick Rycar <nick@buildkite.com>
2021-11-08 10:55:46 -0800

    Just use the local queue

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -2,21 +2,6 @@
 
 set -u
 
-git clone git://github.com/rbenv/rbenv.git ~/.rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-# Install ruby-build as rbenv plugin
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-
-
-rbenv install 2.4.1 && rbenv global 2.4.1
-
-gem install bundler -v 2.2.17
-bundle _2.2.17_ config set --local without 'production'
-bundle _2.2.17_ install
-
 # Mac M1 Build Nonsense
 echo 'npm install libvips'
 npm install libvips
