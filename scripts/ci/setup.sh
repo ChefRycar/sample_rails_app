46fcd626c7be1caae6308ca64dc5e04f91d9ef1d
Nick Rycar <nick@buildkite.com>
2021-11-08 13:28:25 -0800

    double dash for shell commands

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -6,15 +6,15 @@ STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo ohai | sum | cut -f 1 -d \ )
 
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID echo "Ohai!"
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID -- echo "Ohai!"
 ./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START ohai
 
 STEP_START=$(date +%s)
 STEP_SPAN_ID=$(echo nite-nite | sum | cut -f 1 -d \ )
 
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID echo "Yaaaaaawn"
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID sleep 10
-./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID echo "I'm up!"
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID -- echo "Yaaaaaawn"
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID -- sleep 10
+./scripts/ci/buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID -- echo "I'm up!"
 
 ./scripts/ci/buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START nite-nite
 
