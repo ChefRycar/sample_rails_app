5649c1088f9231c0154be26bd9f6a179e6c7eb33
Nick Rycar <nick@buildkite.com>
2021-11-08 11:04:09 -0800

    Send data to honeycomb

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 set -u
 
+BUILD_START=$(date +%s)
+STEP_START=$(date +%s)
+STEP_SPAN_ID=$(echo install | sum | cut -f 1 -d \ )
 # Mac M1 Build Nonsense
 echo 'npm install libvips'
 npm install libvips
@@ -18,6 +20,14 @@ yarn add jquery@3.5.1 bootstrap@3.4.1
 echo 'yarn install'
 yarn install
 
+./buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START install
+
+
+STEP_START=$(date +%s)
+STEP_SPAN_ID=$(echo testing | sum | cut -f 1 -d \ )
+
 bundle exec rake db:migrate
 bundle exec rails test
 bundle exec rspec .
+
+./buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START testing
