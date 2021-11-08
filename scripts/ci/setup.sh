3869248e8ac40a77018cf280b47bf1791da27121
Nick Rycar <nick@buildkite.com>
2021-11-08 11:30:06 -0800

    Initialize KISS mode.

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -3,31 +3,8 @@ set -u
 
 BUILD_START=$(date +%s)
 STEP_START=$(date +%s)
-STEP_SPAN_ID=$(echo install | sum | cut -f 1 -d \ )
-# Mac M1 Build Nonsense
-echo 'npm install libvips'
-npm install libvips
-echo 'npm install sharp'
-npm install sharp --build-from-source
-echo 'npm audit fix'
-npm audit fix
+STEP_SPAN_ID=$(echo ohai | sum | cut -f 1 -d \ )
 
-echo 'npm install yarn'
-npm install yarn
+echo "Ohai!"
 
-echo 'yarn add'
-yarn add jquery@3.5.1 bootstrap@3.4.1
-echo 'yarn install'
-yarn install
-
-./buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START install
-
-
-STEP_START=$(date +%s)
-STEP_SPAN_ID=$(echo testing | sum | cut -f 1 -d \ )
-
-bundle exec rake db:migrate
-bundle exec rails test
-bundle exec rspec .
-
-./buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START testing
+./buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START ohai
